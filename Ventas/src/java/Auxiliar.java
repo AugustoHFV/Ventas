7b887801db022240f65a5ac3db2ca7f73cc2847d
AugustoHFV <124326528+AugustoHFV@users.noreply.github.com>
2023-03-19 22:48:24 -0600

    Add files via upload

---
+++ Ventas/src/java/Auxiliar.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author werna
+ */
+public class Auxiliar {
+    String producto="Galleta (default)";
+    int Precio=10;
+    int Dinero=10000;
+    int disponibilidad=50;
+    int comprado=0;
+
+    public int getComprado() {
+        return comprado;
+    }
+
+    public void setComprado(int comprado) {
+        this.comprado = comprado;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(int Precio) {
+        this.Precio = Precio;
+    }
+
+    public int getDinero() {
+        return Dinero;
+    }
+
+    public void setDinero(int Dinero) {
+        this.Dinero = Dinero;
+    }
+
+    public int getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(int disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+}
